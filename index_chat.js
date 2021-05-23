e018db6ff68f7ca97df19c3e4724f60549e838ef
Olexandr-Sh <shevtcov07@ukr.net>
2021-05-23 16:23:03 +0300

    added text and date blocks

--- index_chat.js
+++ index_chat.js
@@ -1,16 +1,31 @@
 class Chat {
+  constructor() {
+    let sendButton = document.getElementById('sendButton');
+    sendButton.addEventListener('click', () => {
+      this.sendMessage();
+    });
+  }
+
   sendMessage() {
-    let messageHistoryBlock = document.getElementsByClassName(messageHistory);
-    let messageBlock = this.createMessageBlock();
+    let messageHistoryBlock = document.getElementById('messageHistory');
+    let inputElement = document.getElementById('msg');
+
+    let messageBlock = this.createMessageBlock({ text: inputElement.value });
     messageHistoryBlock.appendChild(messageBlock);
   }
 
-  createMessageBlock() {
+  createMessageBlock(params) {
     let messageBlock = document.createElement('div');
     messageBlock.classList.add('message');
 
     let messageDateBlock = this.createMessageDateBlock();
     messageBlock.appendChild(messageDateBlock);
+
+    let messageAvatarBlock = this.createMessageAvatarBlock();
+    messageBlock.appendChild(messageAvatarBlock);
+
+    let messageTextBlock = this.createMessageTextBlock(params.text);
+    messageBlock.appendChild(messageTextBlock);
     
     return messageBlock;
   }
@@ -18,9 +33,23 @@ class Chat {
   createMessageDateBlock() {
     let messageDateBlock = document.createElement('span');
     messageDateBlock.classList.add('date');
-    messageDateBlock.textContent - '23.05.21';
+    messageDateBlock.textContent = '23.05.21';
 
     return messageDateBlock;
+  }
+
+  createMessageAvatarBlock() {
+    let messageAvatarBlock = document.createElement('div');
+    messageAvatarBlock.classList.add('avatar');
+
+    return messageAvatarBlock;
+  }
+
+  createMessageTextBlock(text) {
+    let messageTextBlock = document.createElement('p');
+    messageTextBlock.textContent = text;
+
+    return messageTextBlock;
   }
 }
 
